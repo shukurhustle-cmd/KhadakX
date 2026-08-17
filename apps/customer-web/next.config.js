1952afe736362e5cc38d22772cf84cea5d51c993
shukurhustle-cmd <shukurhustle@gmail.com>
2026-08-17 22:06:12 +0530

    fix(customer-web): remove forced root React aliases

--- apps/customer-web/next.config.js
+++ apps/customer-web/next.config.js
@@ -1,21 +1,8 @@
-const path = require('path');
-
-const root = path.resolve(__dirname, '../..');
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['localhost', 'khakadx.s3.amazonaws.com'],
   },
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      react: path.join(root, 'node_modules/react'),
-      'react-dom': path.join(root, 'node_modules/react-dom'),
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
