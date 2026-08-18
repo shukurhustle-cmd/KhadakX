552888e4154d845d9c78b521c0e3417ab3b3f617
shukurhustle-cmd <shukurhustle@gmail.com>
2026-08-18 15:15:06 +0530

    feat(auth): enable JWT guard in business module

--- apps/api/src/modules/business/business.module.ts
+++ apps/api/src/modules/business/business.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { BusinessController } from './business.controller';
 import { BusinessService } from './business.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [BusinessController],
   providers: [BusinessService],
   exports: [BusinessService],
